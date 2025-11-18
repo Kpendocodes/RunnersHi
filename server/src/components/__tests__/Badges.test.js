db5c65654f64f3319eb29a875f9e39e4c1642201
Ken <kmpoluoch@gmail.com>
2025-11-18 15:25:32 -0500

    Delete server directory

--- server/src/components/__tests__/Badges.test.js
+++
@@ -1,8 +0,0 @@
-const { render, screen } = require('@testing-library/react');
-const Badges = require('../../../../frontend/src/components/Badges');
-
-test('renders Badges component', () => {
-	render(<Badges />);
-	const linkElement = screen.getByText(/badge/i);
-	expect(linkElement).toBeInTheDocument();
-});
